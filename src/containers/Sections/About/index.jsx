52a565b6f406557c8dc9217a28ea056b6fc8f804
hipstermartin <abhi.yalamaddi@gmail.com>
2021-07-24 17:37:56 +0530

    Update index.jsx

--- src/containers/Sections/About/index.jsx
+++ src/containers/Sections/About/index.jsx
@@ -18,9 +18,10 @@ class About extends React.Component {
                     <h5 className="dark-blue-text title">ABOUT ME</h5>
                     <h6 className="dark-blue-text" style={{marginTop:20, marginBottom:40}}>
                         I am a technology enthusiast and a learning developer. 
-                        I am devoted to programming and I am thrilled to code all things inspiring. 
+                        I am trying to solve real-world problems using programming by developing simple applications.
                         <br></br>
                         In addition to my programming skills, I am also in possession of good social and communication skills.
+                        <br></br>
                         On top of that, I am a dedicated worker who enjoys working in a team, making me a good addition to any working team I am put into.
                         I am passionate and eager to learn and deliver high-quality work.
                     </h6>
