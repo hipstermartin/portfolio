159b859278086a4c761a3858dc9141f4e3919c81
hipstermartin <abhi.yalamaddi@gmail.com>
2021-09-19 11:28:57 +0530

    Update index.jsx

--- src/containers/Sections/Intro/index.jsx
+++ src/containers/Sections/Intro/index.jsx
@@ -40,7 +40,7 @@ class Intro extends React.Component {
                         <Button 
                             light
                             external
-                            href="https://drive.google.com/file/d/1bsWekbnHtYROvIsczaY0kdJDBfrgZ7bp/view?usp=sharing"
+                            href="https://drive.google.com/file/d/1dhbVFYDTnfMeSzSZs0mDaljqTLeCyX_S/view?usp=sharing"
                         >
                             <span className="bold dark-blue-text">SEE RESUME</span>
                         </Button>                    
