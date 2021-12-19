a2168f2cc69401f4cbc311ee6c5726f702985047
hipstermartin <abhi.yalamaddi@gmail.com>
2021-12-19 18:59:29 +0530

    Update tools.js

--- src/containers/Sections/About/tools.js
+++ src/containers/Sections/About/tools.js
@@ -1,10 +1,10 @@
 const tools = [
-    'GIT',
-    'ANDROID STUDIO',
-    'LINUX',
-    'Visual Studio',
-    'Eclipse IDE',
-    'Firebase'
+    'REACT',
+    'NODE.JS',
+    'SPRING',
+    'TENSOR FLOW',
+    'OPEN CV',
+    'SPACY'
 ];
 
 export default tools;
