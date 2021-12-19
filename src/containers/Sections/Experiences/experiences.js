72f7d343d0d70804c0b2c8f40ac8dee7e7da0f4e
hipstermartin <abhi.yalamaddi@gmail.com>
2021-12-19 18:51:38 +0530

    Update experiences.js

--- src/containers/Sections/Experiences/experiences.js
+++ src/containers/Sections/Experiences/experiences.js
@@ -2,20 +2,17 @@ const experiences = [
     {
       title:'Software Development Engineer Intern (EFS) at NetApp Inc', 
       period:'Aug 2021 – Present',
-      content:'Currently working with the NFS team on the development of NAS protocols, like NFS and CIFS, also on simplicity projects for protocol access. Involved in development of NFS server stack software for the NetApp proprietary ONTAP operating system kernel.'
+      content:'Working with the NFS(Network File System) team on the development of NAS protocols such as NFS and CIFS, as well as NFS server stack software for NetApp`s proprietary ONTAP operating system kernel.'
     },
     {
       title:'Mobile App development Intern at Thing Tronics Pvt.Ltd.', 
       period:'May 2019 – Jun 2019',
-      content:'Designed and implemented working prototype with multiple algorithms for indoor positioning system using BLE Beacons. Worked on reducing the algorithm precision error by 5% and increased accuracy by 9%. Deployed the algorithm in an android application and tested it in real-time.'
-    },
-    {
-      title:'Cheif Advisor at CSI-VITAP', 
-      period:'Oct 2020 - Sep 2021'
+      content:'Designed and built a working prototype with multiple algorithms for an indoor positioning system using Ble Beacons. The algorithm precision error was reduced by 5% and the accuracy was enhanced by 9%, resulting in a more precise location estimation for the user. In addition, I collaborated with team members to organize modifications and delegate tasks by using version control systems such as Git.'
     },
     {
       title:'Vice-President at CSI-VITAP', 
-      period:'Jul 2018 - Oct 2020'
+      period:'Jul 2018 - Sep 2021',
+      content: 'The Computer Society of India has about 200 members and is a technical chapter(club) at VIT University in AP. As Technical Vice-President, I led and managed the chapters progress. Coordinated multiple tech presentations, hackathons, and workshops.'
     }
 ];
 
