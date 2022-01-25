d68b5e002bd664d70b6731a28f92583b578343b3
hipstermartin <abhi.yalamaddi@gmail.com>
2022-01-25 19:36:31 +0530

    Update experiences.js

--- src/containers/Sections/Experiences/experiences.js
+++ src/containers/Sections/Experiences/experiences.js
@@ -7,12 +7,12 @@ const experiences = [
     {
       title:'Mobile App development Intern at Thing Tronics Pvt.Ltd.', 
       period:'May 2019 – Jun 2019',
-      content:'Designed and built a working prototype with multiple algorithms for an indoor positioning system using Ble Beacons. The algorithm precision error was reduced by 5% and the accuracy was enhanced by 9%, resulting in a more precise location estimation for the user. In addition, I collaborated with team members to organize modifications and delegate tasks by using version control systems such as Git.'
+      content:'Designed and built a working prototype with multiple algorithms for an indoor positioning system using BLE Beacons. Reduced the precision error by 5% and enhanced the accuracy by 9%, which resulted in a more precise location estimation for the user. In addition, I collaborated with team members to organize modifications and delegate tasks by using version control systems such as Git.'
     },
     {
       title:'Vice-President at CSI-VITAP', 
       period:'Jul 2018 - Sep 2021',
-      content: 'The Computer Society of India has about 200 members and is a technical chapter(club) at VIT University in AP. As Technical Vice-President, I led and managed the chapters progress. Coordinated multiple tech presentations, hackathons, and workshops.'
+      content: ' The Computer Society of India is a technical chapter at VIT University, AP with a capacity over 200+ members. And as the Technical Vice-President, I lead and directed the chapter towards the advancement. Conducted handful of practical workshops where I share the technologies I am aware of (that are not part of the curriculum), and also organized multiple tech talks, conducted hackathons, workshops.'
     }
 ];
 
